406c1618742710abfabf46bcd54910c694bb563a
tuhoang <tu.hoang@gosmartlog.com>
2022-09-20 10:43:51 +0700

    update title route

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -5,11 +5,15 @@ export const routes: Route[] = [
     path: 'home',
     loadComponent: () =>
       import('../home/home.component').then((c) => c.HomeComponent),
+    title: 'Home',
   },
   {
     path: 'pokemon-list',
     loadComponent: () =>
-      import('../pokemon-list/pokemon-list.component').then((c) => c.PokemonListComponent),
+      import('../pokemon-list/pokemon-list.component').then(
+        (c) => c.PokemonListComponent
+      ),
+    title: 'Pokemon List',
   },
   {
     path: '',
